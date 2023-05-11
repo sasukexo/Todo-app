06b8ea2ee17f4674206f2f9705d4de9c1d2622d3
Sasukexo <aswinash1002@gmail.com>
2023-05-11 23:08:52 +0530

    changes in html

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'tasks/list.html')
+     return render(request, 'tasks/list.html')
+   
+   
